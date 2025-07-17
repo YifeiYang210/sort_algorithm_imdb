f0cc444ed3de36ad0c49bd4824f2ea1e184f2ea5
YifeiYang210 <yangyifei1998@gmail.com>
2025-07-17 23:15:11 +0800

    task02

--- sort_imdb.py
+++ sort_imdb.py
@@ -1,17 +1,18 @@
-"""
-(一) 存储数据
-1. 读取movies.csv逗号分隔的数据文件的每一行。第一部分是电影片名，第二部分是对应的电影评分;
-2. 以您喜欢的格式存储数据。最终输出中不应有引号，因此请确保电影标题中没有引号;
-3. 打印数据，用 ' - ' 分隔标题和评级，如示例所示。另起一行打印每个影片。
-提示： 使用 encoding=UTF-8 从文件中读取数据。
-
-示例 1：
-The Shawshank Redemption - 9.3
-The Avengers - 8.0
-"""
 import csv
 
-def sort_movies(file_path):
+
+def store_movies(file_path):
+    """
+    (一) 存储数据
+    1. 读取movies.csv逗号分隔的数据文件的每一行。第一部分是电影片名，第二部分是对应的电影评分;
+    2. 以您喜欢的格式存储数据。最终输出中不应有引号，因此请确保电影标题中没有引号;
+    3. 打印数据，用 ' - ' 分隔标题和评级，如示例所示。另起一行打印每个影片。
+    提示： 使用 encoding=UTF-8 从文件中读取数据。
+
+    示例 1：
+    The Shawshank Redemption - 9.3
+    The Avengers - 8.0
+    """
     movies = []
     
     # 读取CSV文件
@@ -24,14 +25,33 @@ def sort_movies(file_path):
                 movies.append((title, rating))
     
     return movies
-    
+
+
+def search_movies(movies, target_rating=6.0):
+    """
+    (二) 线性搜索
+    下一步是对上一阶段的结果集执行线性搜索。实施线性搜索算法，以查找评分为 6 的电影。
+
+    在此阶段，您的程序应执行以下步骤：
+    1. 迭代数据;
+    2. 检查每个元素的评分是否等于 6;
+    3. 如果是，请打印电影标题和评级，如上一阶段所示。确保将 ratings 转换为 float。
+
+    示例 1： 
+    Hercules - 6.0
+    The Fast and the Furious: Tokyo Drift - 6.0
+    Finding You - 6.0
+    """
+    for title, rating in movies:
+        if float(rating) == target_rating:
+            print(f"{title} - {float(rating)}")
+
+
 def main():
     file_path = 'movies.csv'
-    movies = sort_movies(file_path)
+    movies = store_movies(file_path)
 
-    # 打印结果
-    for title, rating in movies:
-        print(f"{title} - {rating}")
+    search_movies(movies, target_rating=6.0)
 
 
 if __name__ == "__main__":
