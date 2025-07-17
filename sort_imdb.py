c17beb128365bf8f8ec728ecb47c59d29e6cb050
YifeiYang210 <yangyifei1998@gmail.com>
2025-07-17 23:22:13 +0800

    task03

--- sort_imdb.py
+++ sort_imdb.py
@@ -47,11 +47,39 @@ def search_movies(movies, target_rating=6.0):
             print(f"{title} - {float(rating)}")
 
 
+def bubble_sort(movies):
+    """
+    (三) 从低到高排序
+    在此阶段，您需要通过实施冒泡排序对数据进行排序。
+    复杂度是O(n^2)，其中n是数据集的大小。同样，请注意您的算法执行需要多长时间。
+
+    您的程序应执行以下操作：
+    1. 比较第一个和第二个元素的评级。如果第一部电影的评分高于第二部，请交换它们;
+    2. 接下来，比较第二个和第三个元素。如果第二个大于第三个，请交换它们。继续比较和交换元素，直到到达列表的最末尾;
+    3. 重复上述步骤 n-1时间，其中n是元素的数量;
+    4. 以示例中所示的格式打印排序的数据。
+
+    示例 1：
+    Hercules - 6.0
+    The Avengers - 8.0
+    The Shawshank Redemption - 9.3
+    """
+    n = len(movies)
+    
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if float(movies[j][1]) > float(movies[j+1][1]):
+                movies[j], movies[j+1] = movies[j+1], movies[j]
+    
+    for title, rating in movies:
+        print(f"{title} - {float(rating)}")
+
 def main():
     file_path = 'movies.csv'
     movies = store_movies(file_path)
 
-    search_movies(movies, target_rating=6.0)
+    # search_movies(movies, target_rating=6.0)
+    bubble_sort(movies)
 
 
 if __name__ == "__main__":
